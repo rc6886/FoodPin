5d7a28aba22573613defe9b4114abe1e23f2ba5c
admin <admin@admins-mac-mini.attlocal.net>
2018-08-29 21:25:00 -0500

    Committing current progress. Added the table view with restaurants, description, and images.

---
+++ FoodPin/RestaurantTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by admin on 8/29/18.
+//  Copyright © 2018 rc6886. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView! {
+        didSet {
+            thumbnailImageView.layer.cornerRadius = 30.0
+            thumbnailImageView.clipsToBounds = true
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
